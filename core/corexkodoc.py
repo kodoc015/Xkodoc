3532d4d9b0cd7934b6b2c649001bb87fda1d2d05
kodoc015 <41295465+kodoc015@users.noreply.github.com>
2018-07-18 04:20:47 +0700

    Update corexkodoc.py

--- core/corexkodoc.py
+++ core/corexkodoc.py
@@ -1,1 +1,328 @@
+## corexkodoc.py - useful module of X-KodoC
+# -*- coding: utf-8 -*-
+import os
+import sys
+import time
 
+xkodoc_banner = """
+.--.   .--.       .--.  .--.         .--.        .------.
+:   .  .  :       :  : .  .          :  :       .   ....:
+ .   ..  .        :   .  ..-----..---:  :.-----.:   .
+  .     .  .----. :         .-.    .-.     .-.      .
+ .   ..  . '----' :   .     : :    : :     : :      '
+:   .  .  :       :  : .    '-'    '-'     '-' :.   '''':
+'--'   '--'       '--'  '-'-----''------''-----' '------'                    
+              
+"""
+backtomenu_banner = """
+  [99] Menu X-KodoC
+  [00] Exit the X-KodoC
+"""
+def restart_program():
+	python = sys.executable
+	os.execl(python, python, * sys.argv)
+	curdir = os.getcwd()
+
+def backtomenu_option():
+	print backtomenu_banner
+	backtomenu = raw_input("xkodoc > ")
+
+	if backtomenu == "99":
+		restart_program()
+	elif backtomenu == "00":
+		sys.exit()
+	else:
+		print "\nERROR: Wrong Input"
+		time.sleep(2)
+		restart_program()
+
+def banner():
+	print xkodoc_banner
+
+def nmap():
+	print '\n###### Installing Nmap'
+	os.system('apt update && apt upgrade')
+	os.system('apt install nmap')
+	print '###### Done'
+	print "###### Type 'nmap' to start."
+	backtomenu_option()
+
+def red_hawk():
+	print '\n###### Installing RED HAWK'
+	os.system('apt update && apt upgrade')
+	os.system('apt install git php')
+	os.system('git clone https://github.com/Tuhinshubhra/RED_HAWK')
+	os.system('mv RED_HAWK ~')
+	print '###### Done'
+	backtomenu_option()
+
+def dtect():
+	print '\n###### Installing D-Tect'
+	os.system('apt update && apt upgrade')
+	os.system('apt install python2 git')
+	os.system('git clone https://github.com/shawarkhanethicalhacker/D-TECT')
+	os.system('mv D-TECT ~')
+	print '###### Done'
+	backtomenu_option()
+
+def sqlmap():
+	print '\n###### Installing sqlmap'
+	os.system('apt update && apt upgrade')
+	os.system('apt install git python2')
+	os.system('git clone https://github.com/sqlmapproject/sqlmap')
+	os.system('mv sqlmap ~')
+	print '###### Done'
+	backtomenu_option()
+
+def sqlmate():
+	print '\n###### Installing sqlmate'
+	os.system('apt update && apt upgrade')
+	os.system('apt install python2 git')
+	os.system('pip2 install mechanize bs4 HTMLparser argparse requests urlparse2')
+	os.system('git clone https://github.com/UltimateHackers/sqlmate')
+	os.system('mv sqlmate ~')
+	print '###### Done'
+	backtomenu_option()
+
+def sqliv():
+	print '\n###### Installing SQLiv'
+	os.system('apt update && apt upgrade')
+	os.system('apt install python2 git')
+	os.system('git clone https://github.com/Hadesy2k/sqliv')
+	os.system('mv sqliv ~')
+	print '###### Done'
+	backtomenu_option()
+	
+def mbf():
+    print '\n###### Installing mbf'
+    os.system('apt update && apt upgrade')
+    os.system('pip2 install mechanize')
+    os.system('git clone https://github.com/pirmansx/mbf')
+    os.system('mv mbf~')
+    print '###### Done'
+    backtomenu_option()	
+
+def sqlscan():
+	print '\n###### Installing sqlscan'
+	os.system('apt update && apt upgrade')
+	os.system('apt install git php')
+	os.system('git clone http://www.github.com/Cvar1984/sqlscan')
+	os.system('mv sqlscan ~')
+	print '###### Done'
+	backtomenu_option()
+	
+def instabot():
+    print '\n###### Installing instabot'
+    os.system('apt update && apt upgrade')
+    os.system('apt install python2')
+    os.system('git clone https://github.com/instagrambot/instabot')
+    os.system('mv instabot ~')
+    print '###### Done'	
+    backtomenu_option()
+    
+def admin_finder():
+    print '\n###### Installing admin-finder'
+    os.system('apt update && apt upgrade')
+    os.system('apt install python2')
+    os.system('git clone https://github.com/the-c0d3r/admin-finder')
+    os.system('mv admin-finder~')
+    print '###### Done'
+    backtomenu_option()
+        
+def brute_gmail():
+    print '\n###### Installing Brute_force gmail'
+    os.system('apt update && apt upgrade')
+    os.system('git clone https://github.com/JamesAndresCM/Brute_force_gmail')
+    os.system('mv Brute_force_gmail~')    
+    print '###### Done'
+    backtomenu_option()
+         
+def spam_tokped():
+    print '\n###### Installing Spam Tokopedia'
+    os.system('apt update && apt upgrade')
+    os.system('apt install unzip')
+    os.system('git clone https://github.com/mrcakil/spam')
+    os.system('mv Spam Tokopedia~')
+    print '###### Done'
+    backtomenu_option()
+           
+def auto_ig():
+    print '\n###### Installing Auto Ig'
+    os.system('apt update && apt upgrade')
+    os.system('apt install js')
+    os.system('git clone https://github.com/anadhelf/ig')
+    os.system('npm install')
+    os.system('mv Auto Ig~')
+    print '###### Done'
+    backtomenu_option()           
+    
+def spam_tlp():
+    print '\n###### Installing spam-tlp'
+    os.system('apt update && apt upgrade')
+    os.system(' git clone https://github.com/RoySans/Spam-TLP')
+    os.system('mv spam-tlp~')
+    print '###### Done'
+    backtomenu_option()  
+
+def wpscan():
+	print '\n###### Installing WPScan'
+	os.system('apt update && apt upgrade')
+	os.system('apt install git ruby curl')
+	os.system('git clone https://github.com/wpscanteam/wpscan')
+	os.system('mv wpscan ~ && cd ~/wpscan')
+	os.system('gem install bundle && bundle config build.nokogiri --use-system-libraries && bundle install && ruby wpscan.rb --update')
+	print '###### Done'
+	backtomenu_option()
+
+def wordpresscan():
+	print '\n###### Installing wordpresscan(2)'
+	os.system('apt update && apt upgrade')
+	os.system('apt install nmap figlet git')
+	os.system('git clone https://github.com/silverhat007/termux-wordpresscan')
+	os.system('cd termux-wordpresscan && chmod +x * && sh install.sh')
+	os.system('mv termux-wordpresscan ~')
+	print '###### Done'
+	print "###### Type 'wordpresscan' to start."
+	backtomenu_option()
+
+def hash_buster():
+	print '\n###### Installing Hash-Buster'
+	os.system('apt update && apt upgrade')
+	os.system('apt install python2 git')
+	os.system('git clone https://github.com/UltimateHackers/Hash-Buster')
+	os.system('mv Hash-Buster ~')
+	print '###### Done'
+	backtomenu_option()
+
+def webdav():
+	print '\n###### Installing Webdav'
+	os.system('apt update && apt upgrade')
+	os.system('apt install python2 openssl curl libcurl')
+	os.system('pip2 install urllib3 chardet certifi idna requests')
+	os.system('mkdir ~/webdav')
+	os.system('curl -k -O http://override.waper.co/files/webdav.txt;mv webdav.txt ~/webdav/webdav.py')
+	print '###### Done'
+	backtomenu_option()
+
+def wpsploit():
+	print '\n###### Installing WPSploit'
+	os.system('apt update && apt upgrade')
+	os.system('apt install python2 git')
+	os.system('git clone git clone https://github.com/m4ll0k/wpsploit')
+	os.system('mv wpsploit ~')
+	print '###### Done'
+	backtomenu_option()
+
+def sqldump():
+	print '\n###### Installing sqldump'
+	os.system('apt update && apt upgrade')
+	os.system('apt install python2 curl')
+	os.system('pip2 install google')
+	os.system('curl -k -O https://gist.githubusercontent.com/Gameye98/76076c9a282a6f32749894d5368024a6/raw/6f9e754f2f81ab2b8efda30603dc8306c65bd651/sqldump.py')
+	os.system('mkdir ~/sqldump && chmod +x sqldump.py && mv sqldump.py ~/sqldump')
+	print '###### Done'
+	backtomenu_option()
+
+def sqlokmed():
+	print '\n###### Installing sqlokmed'
+	os.system('apt update && apt upgrade')
+	os.system('apt install python2 git')
+	os.system('pip2 install urllib2')
+	os.system('git clone https://github.com/Anb3rSecID/sqlokmed')
+	os.system('mv sqlokmed ~')
+	print '###### Done'
+	backtomenu_option()
+
+def metasploit():
+	print '\n###### Installing Metasploit'
+	os.system("apt update && apt upgrade")
+	os.system("apt install git wget curl")
+	os.system("wget https://gist.githubusercontent.com/Gameye98/d31055c2d71f2fa5b1fe8c7e691b998c/raw/09e43daceac3027a1458ba43521d9c6c9795d2cb/msfinstall.sh")
+	os.system("mv msfinstall.sh ~;cd ~;sh msfinstall.sh")
+	print '###### Done'
+	print "###### Type 'msfconsole' to start."
+	backtomenu_option()
+
+def spammer_grab():
+	print '\n###### Installing Spammer-Grab'
+	os.system('apt update && apt upgrade')
+	os.system('apt install python2 git && pip2 install requests')
+	os.system('git clone https://github.com/p4kl0nc4t/spammer-grab')
+	os.system('mv spammer-grab ~')
+	print '###### Done'
+	backtomenu_option()
+
+def hac():
+	print '\n###### Installing Hac'
+	os.system('apt update && apt upgrade')
+	os.system('apt install php git')
+	os.system('git clone https://github.com/Cvar1984/Hac')
+	os.system('mv Hac ~')
+	print '###### Done'
+	backtomenu_option()
+
+def spammer_email():
+	print '\n###### Installing Spammer-Email'
+	os.system("apt update && apt upgrade")
+	os.system("apt install git python2 && pip2 install argparse requests")
+	os.system("git clone https://github.com/p4kl0nc4t/Spammer-Email")
+	os.system("mv Spammer-Email ~")
+	print '###### Done'
+	backtomenu_option()
+
+def ngrok():
+	print '\n###### Installing Ngrok'
+	os.system('apt update && apt upgrade')
+	os.system('apt install git')
+	os.system('git clone https://github.com/themastersunil/ngrok')
+	os.system('mv ngrok ~')
+	print '###### Done'
+	backtomenu_option()
+
+def sudo():
+	print '\n###### Installing sudo'
+	os.system('apt update && apt upgrade')
+	os.system('apt install ncurses-utils git')
+	os.system('git clone https://github.com/st42/termux-sudo')
+	os.system('mv termux-sudo ~ && cd ~/termux-sudo && chmod 777 *')
+	os.system('cat sudo > /data/data/com.termux/files/usr/bin/sudo')
+	os.system('chmod 700 /data/data/com.termux/files/usr/bin/sudo')
+	print '###### Done'
+	backtomenu_option()
+
+def owscan():
+	print '\n###### Installing OWScan'
+	os.system('apt update && apt upgrade')
+	os.system('apt install git php')
+	os.system('git clone https://github.com/Gameye98/OWScan')
+	os.system('mv OWScan ~')
+	print '###### Done'
+	backtomenu_option()
+
+def sanlen():
+	print '\n###### Installing santet-online'
+	os.system('apt update && apt upgrade')
+	os.system('apt install git python2 && pip2 install requests')
+	os.system('git clone https://github.com/Gameye98/santet-online')
+	os.system('mv santet-online ~')
+	print '###### Done'
+	backtomenu_option()
+
+def spazsms():
+	print '\n###### Installing SpazSMS'
+	os.system('apt update && apt upgrade')
+	os.system('apt install git python2 && pip2 install requests')
+	os.system('git clone https://github.com/Gameye98/SpazSMS')
+	os.system('mv SpazSMS ~')
+	print '###### Done'
+	backtomenu_option()
+
+def osif():
+	print '\n###### Installing OSIF'
+	os.system('apt update && apt upgrade')
+	os.system('apt install git python2')
+	os.system('pip2 install requests')
+	os.system('git clone https://github.com/ciku370/OSIF')
+	os.system('mv OSIF ~')
+	print '###### Done'
+	backtomenu_option()
